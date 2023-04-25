a79c98da4184851bafcca9050f6b125f03773695
alexscmachado <coderaxmcaps@gmail.com>
2023-04-25 19:24:23 -0300

    ajuste da imagem

--- src/components/listItens/ListItensContents.js
+++ src/components/listItens/ListItensContents.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Itens from "./Itens";
 
-
+import imagem from "../imagem/batom.png";
 
 // const BatomImage = new URL("./batom.png", import.meta.url)
 
@@ -9,7 +9,7 @@ function ListItensContent(){
     return(
     <div>
     <h1>Eu sou uma lista de itens</h1>
-    <Itens  produto= "Batom" descricao="Batom vermelho" preco="R$ 30,00"/> 
+    <Itens imagem = {imagem} produto= "Batom" descricao="Batom vermelho" preco="R$ 30,00"/> 
     
     </div>
     
