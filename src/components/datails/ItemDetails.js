04017cdc6d645269ecad82b966780690555d0744
Cíntia <111471174+crisCintia@users.noreply.github.com>
2023-05-10 21:10:28 -0300

    Update ItemDetails.js

--- src/components/datails/ItemDetails.js
+++ src/components/datails/ItemDetails.js
@@ -4,39 +4,19 @@ import { Link } from "react-router-dom";
 
 
 function ItemDetails({cardItem}){
-
     return(
-       
-  <div class="container  cardDetailsContainer">
-            
-            <div class="cardDetailImageContent" >
-            <img src={cardItem.imagem}
-              class=" cardDetailsImage"
-              alt="Laptop" />
-            <a href=""></a>
-
-            </div>
-          <div className="cardDetailsItensContent">
-        
-               <p><a href="#!" className= "cardDetailsImage text-dark">{cardItem.nome}</a></p>
-             
-               <p><a href="#!" className=" CardDetailsNome ">{cardItem.preço}</a></p>
-
-               <p> Descrição do produto: {cardItem.description}</p>
-
-               <CountItens/>
+        <div class="container  cardDetailsContainer">
+        <div class="cardDetailImageContent" >
+            <img src={cardItem.imagem} class=" cardDetailsImage" alt="Laptop" /><a href=""></a>
+        </div>
+        <div className="cardDetailsItensContent">
+            <p><a href="#!" className= "cardDetailsImage text-dark">{cardItem.nome}</a></p>
+            <p><a href="#!" className=" CardDetailsNome ">{cardItem.preco}</a></p>7
+            <p> Descrição do produto: {cardItem.descricao}</p>
+            <CountItens/>
               <button type="button" className=" buyButton btn btn-primary">Comprar</button>
+         </div>
           </div>
-          
-          </div>
-        
-   
-  
-
-
-  
-
-
     )
 }
 
