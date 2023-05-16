0a79a44e22525e1b06a5240308f0760be9a43d94
Cintia Soares <cristinacin06@gmail.com>
2023-05-16 16:23:21 -0300

    sincronizar counter

--- src/components/datails/ItemDetails.js
+++ src/components/datails/ItemDetails.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import CountItens from "../listItens/CountItens";
 import { Link } from "react-router-dom";
 
 
-function ItemDetails({cardItem}){
-    return(
+
+function ItemDetails({ cardItem }) {
+    const [ammount, setAmmount] = useState(1);
+
+    const naoAdicionado = true;
+
+    const InputComponent = true ? CountItens : InputComponent;
+
+    function handleOnChangeQtd(qtd) {
+        setAmmount(qtd);
+    }
+
+    return (
         <div className="container  cardDetailsContainer">
-        <div className="cardDetailImageContent" >
-            <img src={cardItem.imagem} className=" cardDetailsImage" alt="Laptop" /><a href=""></a>
+            <div className="cardDetailImageContent" >
+                <img src={cardItem.imagem} className=" cardDetailsImage" alt="Laptop" /><a href=""></a>
+            </div>
+            <div className="cardDetailsItensContent">
+                <p><a href="#!" className="cardDetailsImage text-dark">{cardItem.nome}</a></p>
+                <p className=" CardDetailsNome ">R$ {cardItem.preco}</p>
+                <p> Descrição do produto: {cardItem.descricao}</p>
+                <p> Em estoque: {cardItem.estoque}</p>
+                {/*<p>Quantidade: <CountItens/></p> - PRIMEIRA APLICAÇÃO DA QUANTIDADE
+            <button type="button" className=" buyButton btn btn-primary">Comprar</button>*/}
+                {/* ABAIXO A APLICAÇÃO APÓS A AULA 9 - EVENTOS*/}
+                {naoAdicionado ? <InputComponent
+                    stock={cardItem.estoque}
+                    qtd={ammount}
+                    onChangeQtd={handleOnChangeQtd} /> : <Link to="/cart"><button className="buyButton btn btn-primary">
+                        Ir para o Carrinho</button></Link>}
+                <button className="buyButton btn btn-primary" onClick={() => {
+                    clearInterval(ammount); setAmmount(1); alert(ammount + " Produto(s) adicionado ao carrinho")
+                }}>Adicionar ao carrinho</button>
+            </div>
         </div>
-        <div className="cardDetailsItensContent">
-            <p><a href="#!" className= "cardDetailsImage text-dark">{cardItem.nome}</a></p>
-            <p>R$ <a href="#!" className=" CardDetailsNome ">{cardItem.preco}</a></p>7
-            <p> Descrição do produto: {cardItem.descricao}</p>
-            <CountItens/>
-              <button type="button" className=" buyButton btn btn-primary">Comprar</button>
-         </div>
-         </div>
     )
 }
 
